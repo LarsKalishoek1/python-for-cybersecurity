959697237dc891673d01896ef693ff8ddae8e0e2
arsKalishoek <99069312@mydavinci.nl>
2022-10-20 12:30:07 +0200

    assignment 1

---
+++ insurance.py
@@ -0,0 +1,8 @@
+name = input("What is the name of your company: ")
+emp = input("How many employees does the company have: ")
+loc = input("Where is your company located: ")
+low = input("What is the lowest price for a policy: ")
+high = input("What is the highest price for a policy: ")
+
+
+print(f"We are {name} located in {loc}. Our {emp} employees can help you find the policy that is right for you with policies ranging from {low} to {high} per month!")
